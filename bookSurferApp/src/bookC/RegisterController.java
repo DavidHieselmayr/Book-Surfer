26dd029f5fd9705e513af4a2a30e125abbf20ee7
Fabian Häubl <fabian.haeubl@gmail.com>
2020-05-11 15:34:20 +0200

    Register und Login

--- bookSurferApp/src/bookC/RegisterController.java
+++ bookSurferApp/src/bookC/RegisterController.java
@@ -6,8 +6,8 @@
 package bookC;
 
 import java.io.IOException;
-import java.text.NumberFormat;
 import java.sql.Statement;
+import java.text.NumberFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
@@ -17,38 +17,35 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import loginAndRegisterStuff.InputException;
-import loginAndRegisterStuff.UserLogin;
 import loginAndRegisterStuff.UserLoginRegister;
 
 /**
- * FXML Controller class
  *
- * @author Nexo
+ * @author fabia
  */
+public class RegisterController {
 
-public class StyleBookController {
-    // Verbindung zur Datenbank
-
-    private Statement statement;
+    private final static String VIEWNAME = "styleRegister.fxml";
+    private static final NumberFormat NUMBERFORMAT_2DEC;
+    private static Statement mystatement;
+    private static Stage stage;
+    private static Statement statement;
+    
     @FXML
-    private TextField tfUsername;
-    @FXML
-    private TextField tfPassword;
+    private TextField tfErrorMsg;
     @FXML
     private Button btLogin;
     @FXML
     private Button btRegister;
     @FXML
-    private TextField tfErrorMsg;
-
-    // Helper
-    private final static String VIEWNAME = "styleLogin2.fxml";
-    private static final NumberFormat NUMBERFORMAT_2DEC;
-    private static Statement mystatement;
-    private static Stage stage;
+    private TextField tfUsername;
+    @FXML
+    private TextField tfPassword;
+    @FXML
+    private TextField tfPassword1;
 
     static {
         NUMBERFORMAT_2DEC = NumberFormat.getNumberInstance();
@@ -56,22 +53,11 @@ public class StyleBookController {
         NUMBERFORMAT_2DEC.setMinimumFractionDigits(2);
     }
 
-    /**
-     * Anzeige der View.
-     * <br>
-     * Diese Methode erstellt eine Instanz der View und dieses Controllers
-     * (FXML-Loader) und richtet alles (also vor allem den Controller) so weit
-     * ein, dass es angezeigt werden kann.
-     *
-     * @param stage Stage, in der die View angezeigt werden soll; null, wenn
-     * neue erstellt werden soll.
-     * @param statement Datenbankverbindung
-     */
     public static void show(Stage stage, Statement statement) {
         try {
-            StyleBookController.mystatement = statement;
+            RegisterController.mystatement = statement;
             // View & Controller erstellen
-            FXMLLoader loader = new FXMLLoader(StyleBookController.class.getResource(VIEWNAME));
+            FXMLLoader loader = new FXMLLoader(RegisterController.class.getResource(VIEWNAME));
             Parent root = (Parent) loader.load();
 
             // Scene erstellen
@@ -82,15 +68,15 @@ public class StyleBookController {
                 stage = new Stage();
             }
             stage.setScene(scene);
-            stage.setTitle("BookSurfer");
+            stage.setTitle("BookSurfer-Register");
 
             // Controller ermitteln
-            StyleBookController bookSurferC = (StyleBookController) loader.getController();
+            RegisterController bookSurferRegisterC = (RegisterController) loader.getController();
 
             // Datenbankzugriff merken
-            bookSurferC.statement = statement;
+            bookSurferRegisterC.statement = statement;
 
-            bookSurferC.stage = stage;
+            bookSurferRegisterC.stage = stage;
 
             // View initialisieren
             // lles anzeigen
@@ -110,15 +96,15 @@ public class StyleBookController {
 
     @FXML
     private void actionLogin(ActionEvent event) {
-        try {
-            UserLoginRegister.login(statement, tfUsername.getText(), tfPassword.getText());
-        } catch (InputException ex) {
-            tfErrorMsg.setText(ex.getMessage());
-        }
+        StyleBookController.show(stage, statement);
     }
 
     @FXML
     private void actionRegister(ActionEvent event) {
-        RegisterController.show(stage, statement);
+        try {
+            UserLoginRegister.register(statement, tfUsername.getText(), tfPassword.getText(),tfPassword1.getText());
+        } catch (InputException ex) {
+            tfErrorMsg.setText(ex.getMessage());
+        }
     }
 }
