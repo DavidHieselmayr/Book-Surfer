8f51f4ecf1ed7c3f7bf393df84c50a6226fe91b9
DavidHieselmayr <43745537+DavidHieselmayr@users.noreply.github.com>
2020-05-17 16:04:56 +0200

    reading

---
+++ bookSurferApp/src/bookC/StyleReadingVisionController.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookC;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Nexo
+ */
+public class StyleReadingVisionController implements Initializable {
+
+    @FXML
+    private TextField tfSeite;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void actionFirstPage(ActionEvent event) {
+    }
+
+    @FXML
+    private void actionBackPage(ActionEvent event) {
+    }
+
+    @FXML
+    private void actionForward(ActionEvent event) {
+    }
+
+    @FXML
+    private void actionLastPage(ActionEvent event) {
+    }
+
+    @FXML
+    private void actionMakeFullScreen(ActionEvent event) {
+    }
+
+    @FXML
+    private void actionExit(ActionEvent event) {
+    }
+    
+}
